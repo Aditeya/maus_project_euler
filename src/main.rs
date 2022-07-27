620c949a5c1aea80119437d29bb4c7bbba94aa85
aditeya <22963960+Aditeya@users.noreply.github.com>
2022-07-27 22:58:44 +0530

    improvement to problem 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod solutions;
 
 fn main() {
-    let ans = solutions::p1::s_v1(&999);
+    let ans = solutions::p1::s_v1(&1000);
     println!("ans: {}",ans);
 }
--- src/solutions/p1.rs
+++ src/solutions/p1.rs
@@ -1,5 +1,6 @@
 pub fn s_v1(n: &u32) -> u32 {
-    sum_divisible_by_n(&3, n) + sum_divisible_by_n(&5, n) - sum_divisible_by_n(&15, n)
+    let n = n - 1;
+    sum_divisible_by_n(&3, &n) + sum_divisible_by_n(&5, &n) - sum_divisible_by_n(&15, &n)
 }
 
 fn sum_divisible_by_n(n: &u32, value: &u32) -> u32 {
