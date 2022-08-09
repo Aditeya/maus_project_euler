57c34b57c3fdac3b80757f669cba61d794f7650e
aditeya <22963960+Aditeya@users.noreply.github.com>
2022-08-09 10:35:44 +0530

    Figured out error in problem 15:
    check https://stackoverflow.com/questions/73286662/why-does-x-x-y-give-a-different-result-from-x-y-in-rust
    for details
    
    > Because `*` and `/` have the same precedence with left associativity, the expression is not `x * ((*n + i) / i)` (which is the same as `x *= (*n + i) / i)` but `(x * (*n + i)) / i`.

--- src/solutions/p15.rs
+++ src/solutions/p15.rs
@@ -1,8 +1,10 @@
 pub fn s_v1(n: &u64) -> u64 {
     let mut result: u64 = 1;
 
+    // NOTE: formula is || (n+i)/i . answer is written this way to prevent precendence errors
     for i in 1..=*n  {
-        result = result * (*n + i) / i; // NOTE: *= gives wrong answer for some reason
+        result *= *n + i; 
+        result /= i;
     }
 
     result
