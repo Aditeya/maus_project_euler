e2e4ba4b36b074f1f7a61ca54a6100d657715c4a
aditeya <22963960+Aditeya@users.noreply.github.com>
2022-08-15 12:46:20 +0530

    Added table alignment
    - headers centered
    - numbers right aligned

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
 use std::env;
-use std::time::Instant;
 use std::process;
+use std::time::Instant;
 
-use tabled::{TableIteratorExt, Tabled, Style};
+use tabled::{
+    object::{Rows, Segment},
+    Alignment, Modify, Style, TableIteratorExt, Tabled,
+};
 
 mod solutions;
 
 #[derive(Tabled)]
 struct TableResult {
+    #[tabled(rename = "Problem Number")]
     problem_number: u32,
+    #[tabled(rename = "Solution")]
     solution: u64,
+    #[tabled(rename = "time")]
     time: u128,
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    match args.len() {
-        2 => {
-            match args[1].as_str() {
-                "--all" => run_all(),
-                other => {
-                    match other.parse() {
-                        Ok(n) => run_solution(&n),
-                        _ => help()
-                    }
-                }
-            }
-        },
-        _ => help()
-    }
 
+    match args.len() {
+        2 => match args[1].as_str() {
+            "--all" => run_all(),
+            other => match other.parse() {
+                Ok(n) => run_solution(&n),
+                _ => help(),
+            },
+        },
+        _ => help(),
+    }
 }
 
 fn help() {
@@ -48,8 +49,8 @@ fn run_solution(problem_number: &u32) {
             println!(" problem: {}", problem_number);
             println!("solution: {}", solution);
             println!("    time: {} ns", time);
-        },
-        None =>  {
+        }
+        None => {
             println!("Problem Not Solved Yet :(");
             process::exit(1);
         }
@@ -59,22 +60,23 @@ fn run_solution(problem_number: &u32) {
 fn run_all() {
     let mut results: Vec<TableResult> = Vec::new();
     for (problem_number, solution_function) in solutions::SOLUTION_FUNCTIONS_HASHMAP.entries {
-            let now = Instant::now();
-            let solution = solution_function();
-            let time = now.elapsed().as_nanos();
+        let now = Instant::now();
+        let solution = solution_function();
+        let time = now.elapsed().as_nanos();
 
-            results.push(
-                TableResult { 
-                    problem_number: *problem_number,
-                    solution,
-                    time,
-                }
-            );
+        results.push(TableResult {
+            problem_number: *problem_number,
+            solution,
+            time,
+        });
     }
 
-    results.sort_by(|a,b| a.problem_number.cmp(&b.problem_number));
-    let table = results.table()
+    results.sort_by(|a, b| a.problem_number.cmp(&b.problem_number));
+    let table = results
+        .table()
         .with(Style::modern())
+        .with(Modify::new(Segment::all()).with(Alignment::right()))
+        .with(Modify::new(Rows::first()).with(Alignment::center()))
         .to_string();
-    println!("{}",table);
+    println!("{}", table);
 }
